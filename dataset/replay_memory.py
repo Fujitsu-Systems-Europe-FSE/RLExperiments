6da19b94f0b44eec398d8c0ebb95bfc218e57a12
Julien Wallart <julien.wallart@outlook.com>
2023-05-25 16:41:21 +0200

    Refactored code

--- dataset/replay_memory.py
+++ dataset/replay_memory.py
@@ -15,7 +15,13 @@ class ReplayMemory:
         self.memory.append(Transition(*args))
 
     def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
+        transitions_list = random.sample(self.memory, batch_size)
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        # batch = Transition(*zip(*transitions_list))
+        state, action, next_state, reward = map(tuple, zip(*transitions_list))
+        return action, next_state, reward, state
 
     def __len__(self):
         return len(self.memory)
